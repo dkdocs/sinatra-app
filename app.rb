9c03ff85f209cdb3c85772e9e9e4184730893fad
dkdocs <d.k.khatri2012@gmail.com>
2020-02-25 15:30:34 +0530

    minor fixes

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 require 'sinatra'
 require 'net/http'
+require 'uri'
 require 'openssl'
 require "sinatra/json"
-
+require 'pry-rails'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 class Application < Sinatra::Base
@@ -12,12 +13,14 @@ class Application < Sinatra::Base
     uri = URI("#{params[:server]}/trusted")
     request = Net::HTTP::Post.new(uri)
     request.content_type = "application/x-www-form-urlencoded"
+    request["Origin"] = "10.55.97.212"
+    request["Upgrade-Insecure-Requests"] = "1"
+    request["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
    
     request.set_form_data(
         "server" => params[:server],
         "target_site" => params[:site_name],
-        "username" => params[:user_name],
-        "client_ip" => '3.136.78.188'
+        "username" => params[:username],
       )
 
     req_options = {
